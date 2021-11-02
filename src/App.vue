d5507b6ba2a78a720f04cb62e704bfa3bb1b00df
Vayzer <iridescenthao@outlook.com>
2021-11-02 16:18:32 +0800

    lastcode

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
       <router-link to="/code">Code</router-link>
-      <h3></h3>
     </div>
     <router-view/>
   </div>
